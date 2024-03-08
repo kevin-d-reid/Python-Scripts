8bfe58262dfd805b618610d6ddd264d6046a92e9
Kevin D Reid <114093571+kevin-d-reid@users.noreply.github.com>
2024-03-07 19:17:12 -0800

    Upload SMTP Server Testing script + README

---
+++ SMTP Server Testing/README.md
@@ -0,0 +1,3 @@
+# SMTP Server Testing script
+
+Basic script I threw together one morning to probe a remote mail server. Can login to SMTP mailboxes (unencrypted + SSL encrypted) and select both envelope (RFC5321) and message (RFC5322) headers.
---
+++ SMTP Server Testing/SMTPtester.py
@@ -0,0 +1,51 @@
+###################################################################
+# SMTP server tester v1.0
+# Written by Kevin D. Reid
+# Requirements: pyinputplus
+###################################################################
+
+import sys, pyinputplus, smtplib
+from datetime import datetime
+
+# Mail server login fields
+SMTP_username = pyinputplus.inputEmail(prompt='Enter SMTP login email: ')
+SMTP_password = pyinputplus.inputPassword(prompt='Enter SMTP login password: ')
+SMTP_server = pyinputplus.inputURL(prompt='Enter SMTP server name: ')
+print('SMTP server ports: 25 or 1025 for unencrypted, 465 for SSL, 587 for STARTTLS')
+SMTP_port = int(pyinputplus.inputMenu(['25', '1025', '465', '587'], prompt='Enter SMTP server port: \n'))
+
+# Mail server login
+if SMTP_port == 465:
+    server = smtplib.SMTP_SSL(SMTP_server, SMTP_port)
+elif SMTP_port == 25 or 1025 or 587:
+    server = smtplib.SMTP(SMTP_server, SMTP_port)
+try:
+    server.ehlo()
+    if SMTP_port == 587:
+        server.starttls()
+        server.ehlo()
+    server.login(SMTP_username, SMTP_password)
+except Exception as exception:
+    print(exception)
+    sys.exit(1)
+
+# Email sending fields
+envelope_from = pyinputplus.inputEmail(prompt='Enter address for envelope "MAIL FROM:" header: ')
+envelope_to = pyinputplus.inputEmail(prompt='Enter address for envelope "RCPT TO:" header: ')
+email_from = pyinputplus.inputEmail(prompt='Enter address for email "From:" header: ')
+email_to = pyinputplus.inputEmail(prompt='Enter address for email "To:" header: ')
+email_replyto = pyinputplus.inputEmail(prompt='Enter address for email "Reply-To:" header (optional): ', blank=True)
+email_sender = pyinputplus.inputEmail(prompt='Enter address for email "Sender:" header (optional): ', blank=True)
+
+# Email composition, sending, and exiting of script
+date = datetime.now().strftime( "%d/%m/%Y %H:%M" )
+message_subject = 'Test message'
+message_text = 'Test message sent via Python testing script.'
+message = 'From: %s\nReply-To: %s\nSender: %s\nTo: %s\nSubject: %s\nDate: %s\n\n%s' % ( email_from, email_replyto, email_sender, email_to, message_subject, date, message_text)
+try:
+    server.sendmail(envelope_from, envelope_to, message)
+except Exception as exception:
+    print(exception)
+    sys.exit(1)
+server.quit()
+sys.exit()
